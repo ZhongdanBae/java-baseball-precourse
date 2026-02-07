1dd03e1f6efc1bda7e778df995d91ac5e5f67050
zhongdan-b <remi.ni@kakaocorp.com>
2026-02-07 19:09:08 +0900

    feat: 입출력 기능 구현

--- src/main/java/view/Inputview.java
+++ src/main/java/view/Inputview.java
@@ -1,4 +1,15 @@
 package view;
 
+import java.util.Scanner;
+
 public class Inputview {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public String readNumber() {
+        return scanner.nextLine();
+    }
+
+    public String readRestart() {
+        return scanner.nextLine();
+    }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,4 +1,23 @@
 package view;
 
 public class OutputView {
+    public void printInputGuide() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    public void printResult(String msg) {
+        System.out.println(msg);
+    }
+
+    public void printGameEnd() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 시마이");
+    }
+
+    public void printRestartGuide() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    public void printError(String msg) {
+        System.out.println("[ERROR] " + msg);
+    }
 }
